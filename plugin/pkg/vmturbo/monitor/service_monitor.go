049f881b66544923144d51f293736338ef5c8fae
Dongyi Yang <dongyi.yang@vmturbo.com>
2016-04-15 15:33:52 -0400

    Cleaned Some Unnecessary Log

--- plugin/pkg/vmturbo/monitor/service_monitor.go
+++ plugin/pkg/vmturbo/monitor/service_monitor.go
@@ -5,8 +5,6 @@ import (
 
 	vmtproxy "k8s.io/kubernetes/pkg/proxy/vmturbo"
 	vmtAdvisor "k8s.io/kubernetes/plugin/pkg/vmturbo/cadvisor"
-
-	"github.com/golang/glog"
 )
 
 type ServiceMonitor struct{}
@@ -15,13 +13,11 @@ func (self *ServiceMonitor) GetServiceTransactions(host vmtAdvisor.Host) (transa
 	url := fmt.Sprintf("http://%s:%d/", host.IP, 2222)
 	client, err := NewServiceMonitorClient(url)
 	if err != nil {
-		glog.Errorf("Failed to create service monitor client: %s", err)
 		return nil, fmt.Errorf("Failed to create service monitor client: %s", err)
 	}
 	transactions, err = client.TransactionInfo()
 	if err != nil {
-		glog.Errorf("failed to get stats from cadvisor %q - %v\n", url, err)
-		return nil, fmt.Errorf("failed to get stats from cadvisor %q - %v\n", url, err)
+		return nil, fmt.Errorf("failed to get stats from K8s service network monitor %q - %v\n", url, err)
 	}
 	return
 }
